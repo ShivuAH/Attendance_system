1fa7805a8874d26b870945c95967a75fef40832a
ShivuAH <shivanandhanchinal5@gmail.com>
2025-09-09 15:51:27 +0530

    Logo and name set

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# from tkinter import *
-# from tkinter import ttk
-# from PIL import Image,ImageTk
-
-# class Attendence_system:
-#     def __init__(self,root):
-#         self.root=root
-#         self.root.geometry("1530x790+0+0")
-#         self.root.title("Attendece system")
-
-#         #Baground Image
-#         bg_img=Image.open(r"C:\Users\shiva\OneDrive\Desktop\Images\alvas_logo.jpeg")
-#         bg_img=bg_img.resize((1530,710),Image.Resampling.LANCZOS)
-
-#         self.photo_bg_img=ImageTk.PhotoImage(bg_img)
-
-#         bground_img=Label(self.root,image=self.photo_bg_img)
-#         bground_img.place(x=250,y=250,widnith=1530,height=710)
-
-
-
-
-
-# if __name__=="__main__":
-#     root=Tk()
-#     obj=Attendence_system(root)
-#     root.mainloop()
-
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
@@ -37,25 +9,26 @@ class Attendence_system:
         self.root.title("Attendance System")
 
         # Background Image (with transparency effect)
-        bg_img = Image.open(r"C:\Users\shiva\OneDrive\Desktop\Images\alvas_logo.jpeg").convert("RGBA")
-        
+        bg_img = Image.open(r"C:\Users\shiva\OneDrive\Desktop\Images\Engineering.jpg") 
         # Resize to fit window
         bg_img = bg_img.resize((1530, 790), Image.Resampling.LANCZOS)
-
-        # Apply transparency (adjust alpha value: 255 = solid, 0 = fully transparent)
-        alpha = 80  # you can try 80-150 for different transparency levels
-        new_data = []
-        for item in bg_img.getdata():
-            # item = (R, G, B, A)
-            new_data.append((item[0], item[1], item[2], alpha))
-        bg_img.putdata(new_data)
-
         # Convert for Tkinter
         self.photo_bg_img = ImageTk.PhotoImage(bg_img)
 
         # Place in the center
         bground_img = Label(self.root, image=self.photo_bg_img)
         bground_img.place(x=0, y=0, width=1530, height=790)
+
+        logo_img = Image.open(r"C:\Users\shiva\OneDrive\Desktop\Images\alvas-org-logo.png")
+        logo_img = logo_img.resize((117, 131), Image.Resampling.LANCZOS)
+        self.photo_logo_img=ImageTk.PhotoImage(logo_img)
+
+        logo = Label(self.root, image=self.photo_logo_img)
+        logo.place(x=70, y=70, width=117, height=131)
+
+
+        title_lable=Label(bground_img,text="Alvas Institute of Engineering and Technology",font=("Arial black",40,"bold"),fg="red")
+        title_lable.place(x=0,y=0,width=1530,height=55)
 
 
 if __name__ == "__main__":
